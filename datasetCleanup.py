2dac4ec8d2bff147a960d0498ed0abbdcb5fd7d8
Cameron Burrows <cburrows@ufl.edu>
2021-11-14 15:02:54 -0500

    cleanup improvements

--- datasetCleanup.py
+++ datasetCleanup.py
@@ -3,12 +3,36 @@ from natsort import natsorted
 
 path = 'dataset'
 
+
+def is_image(filename, verbose=False):
+
+    data = open(filename,'rb').read(10)
+
+    # check if file is JPG or JPEG
+    if data[:3] == b'\xff\xd8\xff':
+        if verbose == True:
+             print(filename+" is: JPG/JPEG.")
+        return True
+
+    # check if file is PNG
+    if data[:8] == b'\x89\x50\x4e\x47\x0d\x0a\x1a\x0a':
+        if verbose == True:
+             print(filename+" is: PNG.")
+        return True
+
+    # check if file is GIF
+    if data[:6] in [b'\x47\x49\x46\x38\x37\x61', b'\x47\x49\x46\x38\x39\x61']:
+        if verbose == True:
+             print(filename+" is: GIF.")
+        return True
+
+    return False
+
 image_paths = []
 for subdir, dirs, files in os.walk(path):
     for filename in files:
         filepath = subdir + os.sep + filename
-        filepath_lower = filepath.lower()
-        if filepath_lower.endswith(".jpg") or filepath_lower.endswith(".png"):
+        if is_image(filepath):
             image_paths.append(filepath)
         elif os.path.isfile(os.path.abspath(filepath)):
             os.remove(filepath)
@@ -20,7 +44,13 @@ def get_file_parts(path):
     file_parts = os.path.splitext( fname )
     number = file_parts[1].replace('.','')
     name = file_parts[0]
+    try:
+        number_int = int(number)
+    except ValueError:
+        print("Invalid int at: " + path)
     return name, int(number)
+
+
 
 image_paths = natsorted(image_paths)
 last_name = ""
@@ -30,7 +60,10 @@ for img in image_paths:
         last_name = curr_name
         last_num = -1
     if last_num + 1 != curr_num:
-        os.rename(img,img.replace(str(curr_num),str(last_num + 1)))
+        new_name = img.replace(str(curr_num),str(last_num + 1))
+        os.rename(img, new_name.lower())
         print("found: " + img)
+    else:
+        os.rename(img, img.lower())
     last_num += 1
-    os.rename(img, img.lower())
+    
